831b503c37bd66d9e8cb6b1c5cf3947db166e8ba
Muhammad Rakha Nasjaya <muhammadrakha3704@gmail.com>
2025-09-15 18:19:26 +0700

    google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,11 +11,11 @@ const poppins = Poppins({
 export const metadata = {
   title: "Jadiinworks",
   description: "Digital solutions that matter - Web Development, Mobile Apps, UI/UX Design, Automation Bot",
-    icons: {
-      icon: '/Assets/Logomark.png',
-      shortcut: '/Assets/Logomark.png',
-      apple: '/Assets/Logomark.png',
-    },
+  icons: {
+    icon: '/Assets/Logomark.png',
+    shortcut: '/Assets/Logomark.png',
+    apple: '/Assets/Logomark.png',
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -29,6 +29,19 @@ export default function RootLayout({ children }) {
     <html lang="en" suppressHydrationWarning>
       <head>
         <script dangerouslySetInnerHTML={{ __html: initTheme }} />
+        {/* Google Analytics script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-FMBSHQVWRN"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-FMBSHQVWRN');
+        `}
+        </Script>
         <link rel="icon" href="/Assets/Logomark.png" type="image/png" />
         <style dangerouslySetInnerHTML={{
           __html: `
